688078fc9c4cf546b2a73bec9b8184a0c875f924
workunhard <workun.code@gmail.com>
2022-10-02 22:37:02 -0700

    Implemented authentication + SecondActivity to demo user data retrieval.

--- app/src/main/java/com/example/planer/MainActivity.java
+++ app/src/main/java/com/example/planer/MainActivity.java
@@ -1,13 +1,90 @@
 package com.example.planer;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+        }
+    }
+
+    public void signUp(View view) {
+        EditText username = findViewById(R.id.username_input);
+        String user = username.getText().toString();
+        EditText password = findViewById(R.id.password_input);
+        String pass = password.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(user, pass)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(MainActivity.this, "Account created. Please log in.", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(MainActivity.this, "username must follow *@*.com format", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    public void signIn(View view) {
+        EditText username = findViewById(R.id.username_input);
+        String user = username.getText().toString();
+        EditText password = findViewById(R.id.password_input);
+        String pass = password.getText().toString();
+
+        if (!validateForm(user, pass)) {
+            Toast.makeText(MainActivity.this, "Please enter email and password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(user, pass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                    startActivity(intent);
+
+                } else {
+                    Toast.makeText(MainActivity.this, "Incorrect email / password.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public boolean validateForm(String username, String password) {
+        return !username.isEmpty() && !password.isEmpty();
+    }
+
 }
---
+++ app/src/main/java/com/example/planer/SecondActivity.java
@@ -0,0 +1,28 @@
+package com.example.planer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SecondActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        FirebaseUser user = mAuth.getInstance().getCurrentUser();
+
+        TextView currUser = (TextView)findViewById(R.id.userID);
+        String email = user.getEmail();
+        String uid = user.getUid();
+        String result = "Email:\n\t" + email + "\n\n" + "UserID:\n\t" + uid + "\n\n";
+        currUser.setText(result);
+
+    }
+}
