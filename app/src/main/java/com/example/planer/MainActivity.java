cb1b75c28aca0a95091d500ea76ef221d68f6591
Kelly Park <sehyun.kelly.park@gmail.com>
2022-10-03 10:24:10 -0700

    countries info added to firestore

--- app/src/main/java/com/example/planer/MainActivity.java
+++ app/src/main/java/com/example/planer/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.planer;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,20 +9,74 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
+    public static int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
+
+        db = FirebaseFirestore.getInstance();
+
+        // Add countries info do "countries" collection
+        // completed but left for future purposes
+//        Map<String, String> countries = new HashMap<>();
+//
+//        InputStream inputStream = getBaseContext().getResources().openRawResource(R.raw.info);
+//        BufferedReader bufferedReader= new BufferedReader(new InputStreamReader(inputStream));
+//        try{
+//            String line = bufferedReader.readLine();
+//            while (line != null) {
+//                line = bufferedReader.readLine();
+//                String info[] = line.split("/");
+//                countries.put("name", info[0]);
+//                countries.put("airport", info[1]);
+//                countries.put("advisory", info[2]);
+//
+//                db.collection("countries")
+//                        .add(countries)
+//                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+//                            @Override
+//                            public void onSuccess(DocumentReference documentReference) {
+//                                Log.d(TAG, "Count: " + count + ", DocumentSnapshot added with ID: " + documentReference.get());
+//                            }
+//                        }).addOnFailureListener(new OnFailureListener() {
+//                            @Override
+//                            public void onFailure(@NonNull Exception e) {
+//                                Log.w(TAG, "Error adding document", e);
+//                            }
+//                        });
+//
+//                count++;
+//            }
+//            bufferedReader.close();
+//            inputStream.close();
+//        }catch (Exception e){
+//            System.out.println("FileRead Error");
+//            e.printStackTrace();
+//        }
     }
 
     @Override
