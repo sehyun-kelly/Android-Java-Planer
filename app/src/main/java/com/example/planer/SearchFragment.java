e3e988af4f804c738d1e125a5992c9eb377d9372
workunhard <workun.code@gmail.com>
2022-10-09 10:25:54 -0700

    Connected WeatherActivity to Search fragment, playing around with carousel views using motionlayout

--- app/src/main/java/com/example/planer/SearchFragment.java
+++ app/src/main/java/com/example/planer/SearchFragment.java
@@ -1,18 +1,39 @@
 package com.example.planer;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 
 public class SearchFragment extends Fragment {
-
 
     public SearchFragment() {
         super(R.layout.fragment_search);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // OnClickListeners to switch to new activities when clicking cards (Visa, Covid, Weather)
+        CardView visa = requireView().findViewById(R.id.visaCard);
+        visa.setOnClickListener(this::gotoWeather);
+
+        CardView travelRestrictions = requireView().findViewById(R.id.travelRestrictionsCard);
+        travelRestrictions.setOnClickListener(this::gotoWeather);
+
+        CardView weather = requireView().findViewById(R.id.weatherCard);
+        weather.setOnClickListener(this::gotoWeather);
+
+    }
+
+    public void gotoWeather(View view) {
+        Intent intent = new Intent(getActivity(), WeatherActivity.class);
+        startActivity(intent);
+    }
 }
---
+++ app/src/main/java/com/example/planer/WeatherActivity.java
@@ -0,0 +1,16 @@
+package com.example.planer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.helper.widget.Carousel;
+
+import android.os.Bundle;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+    }
+
+}
