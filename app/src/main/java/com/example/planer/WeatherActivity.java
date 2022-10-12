d0230db52cd81ceb10ae69431d7b95c398532afb
workunhard <workun.code@gmail.com>
2022-10-11 23:09:56 -0700

    Added comments to describe JSONObject handling in Java using Volley and OpenWeatherAPI.

--- app/src/main/java/com/example/planer/WeatherActivity.java
+++ app/src/main/java/com/example/planer/WeatherActivity.java
@@ -1,7 +1,6 @@
 package com.example.planer;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,9 +8,6 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -22,8 +18,9 @@ import com.bumptech.glide.Glide;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.text.DecimalFormat;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class WeatherActivity extends AppCompatActivity {
@@ -31,12 +28,14 @@ public class WeatherActivity extends AppCompatActivity {
     EditText etCity, etCountry;
     TextView tvResult;
     ImageView ivIcon;
+    TextView temperature;
     DecimalFormat df = new DecimalFormat("#.#");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+
         TextView date = findViewById(R.id.currentDate);
         DateFormat dateInstance = SimpleDateFormat.getDateInstance();
         String dateStr = dateInstance.format(Calendar.getInstance().getTime());
@@ -46,6 +45,7 @@ public class WeatherActivity extends AppCompatActivity {
         etCity = findViewById(R.id.city_name);
         tvResult = findViewById(R.id.weatherResults);
         ivIcon = findViewById(R.id.icon);
+        temperature = findViewById(R.id.temperature);
     }
 
     public void getWeather(View view) {
@@ -56,37 +56,56 @@ public class WeatherActivity extends AppCompatActivity {
             Toast.makeText(WeatherActivity.this,
                     R.string.noCity, Toast.LENGTH_SHORT).show();
         } else {
+            // City has been entered, init key + URL base
             String APPID = "6ce8f403d3cbee49efdb370cafcc65c3";
             String URL = "http://api.openweathermap.org/data/2.5/weather";
+            // complete URL
             if (!country.equals("")) {
                 tempUrl = URL + "?q=" + city + "," + country + "&appid=" + APPID;
             } else {
                 tempUrl = URL + "?q=" + city + "&appid=" + APPID;
             }
+            /* StringRequest obj via Volley to get JSON as string
+            (read as: 'GET' from 'tempUrl' the 'response'; a string)
+             */
             StringRequest stringRequest = new StringRequest(Request.Method.GET,
                     tempUrl, response -> {
-                        Log.d("response", response);
+                        Log.d("response", response); /* Copy this giant 1 ln string from Logcat
+                        and paste into an online JSON object viewer (e.g., http://jsonviewer.stack.hu/)
+                        to see obj structure
+                        */
                         String output = "";
                         try {
-                            String iconUrl = "http://openweathermap.org/img/wn/";  // + @2x.png
+                            /* Create new JSONObjects and JSONArrays as necessary
+                            (see online JSON object to verify array vs object)
+                             */
                             JSONObject jsonResponse = new JSONObject(response);
                             JSONArray jsonArray = jsonResponse.getJSONArray("weather");
                             JSONObject jsonObjectWeather = jsonArray.getJSONObject(0);
+                            JSONObject jsonObjectMain = jsonResponse.getJSONObject("main");
+                            JSONObject jsonObjectWind = jsonResponse.getJSONObject("wind");
+                            JSONObject jsonObjectClouds = jsonResponse.getJSONObject("clouds");
+
+                            /* Get the icon code jsonObjectWeather, plug it into the necessary URL,
+                            use Glide library to easily replace the img in ImageView with
+                            the image at the URL
+                             */
                             String iconCode = jsonObjectWeather.getString("icon");
+                            String iconUrl = "http://openweathermap.org/img/wn/";
                             iconUrl += iconCode + "@4x.png";
                             Glide.with(this).load(iconUrl).into(ivIcon);
+
+                            // Get and store relevant data from your JSONObjects
                             String description = jsonObjectWeather.getString("description");
-                            JSONObject jsonObjectMain = jsonResponse.getJSONObject("main");
                             double temp = jsonObjectMain.getDouble("temp") - 273.15;
                             double feelsLike = jsonObjectMain.getDouble("feels_like") - 273.15;
                             float pressure = jsonObjectMain.getInt("pressure");
                             int humidity = jsonObjectMain.getInt("humidity");
-                            JSONObject jsonObjectWind = jsonResponse.getJSONObject("wind");
                             String wind = jsonObjectWind.getString("speed");
-                            JSONObject jsonObjectClouds = jsonResponse.getJSONObject("clouds");
                             String clouds = jsonObjectClouds.getString("all");
                             String city1 = jsonResponse.getString("name");
-                            TextView temperature = findViewById(R.id.temperature);
+
+                            // Set views' values using data gathered above
                             String tempStr = "" + df.format(temp) + "°C";
                             temperature.setText(tempStr);
                             output += "Current weather in " + city1 + ":\n" +
@@ -96,13 +115,15 @@ public class WeatherActivity extends AppCompatActivity {
                                     "\nHumidity:     " + humidity + "%" +
                                     "\n\nWind:           " + wind + " m/s" +
                                     "\nClouds:        " + clouds + "%";
-                          tvResult.setText(output);
+                            tvResult.setText(output);
 
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
                     }, error -> Toast.makeText(getApplicationContext(),
                     error.toString().trim(), Toast.LENGTH_SHORT).show());
+
+            // https://google.github.io/volley/simple.html
             RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
             requestQueue.add(stringRequest);
         }
