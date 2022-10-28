d01b97d16428e279e7b4300744da1c1433fe27cc
workunhard <workun.code@gmail.com>
2022-10-27 23:55:21 -0700

    Implemented Firestore Storage to upload profile pic; buggy and doesn't update relevant views on 'Save Changes' in ChangeProfilePic.java.

---
+++ app/src/main/java/com/example/planer/ChangeProfilePic.java
@@ -0,0 +1,102 @@
+package com.example.planer;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class ChangeProfilePic extends AppCompatActivity {
+
+    CircleImageView profilePic;
+    Uri imageUri;
+    Button selectImageBtn;
+    Button saveChangesBtn;
+
+    FirebaseUser user;
+    FirebaseStorage storage;
+    StorageReference storageRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_profile_pic);
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        storage = FirebaseStorage.getInstance();
+        storageRef = storage.getReference();
+        StorageReference picRef = storageRef.child("ProfilePics").child(user.getUid());
+
+        profilePic = findViewById(R.id.profilePic);
+        Glide.with(this).load(picRef).centerCrop().into(profilePic);
+        selectImageBtn = findViewById(R.id.selectImageBtn);
+        saveChangesBtn = findViewById(R.id.saveChangesBtn);
+        selectImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
+        saveChangesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadImage();
+            }
+        });
+    }
+
+    private void selectImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 100);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100 && data != null && data.getData() !=null) {
+            imageUri = data.getData();
+            profilePic.setImageURI(imageUri);
+        }
+    }
+
+
+    private void uploadImage() {
+        String filename = user.getUid();
+        storageRef = FirebaseStorage.getInstance().getReference("ProfilePics/" + filename);
+        if (imageUri != null) {
+            storageRef.putFile(imageUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Toast.makeText(getApplicationContext(), "Profile Picture Changed", Toast.LENGTH_SHORT).show();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getApplicationContext(), "Upload Failed", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        } else {
+            Toast.makeText(this, "Please select a new image before saving", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
--- app/src/main/java/com/example/planer/ProfileFragment.java
+++ app/src/main/java/com/example/planer/ProfileFragment.java
@@ -1,14 +1,21 @@
 package com.example.planer;
 
+import static android.app.Activity.RESULT_OK;
+
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.MediaStore;
+import android.transition.CircularPropagation;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,17 +25,32 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.IOException;
+import java.net.URL;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileFragment extends Fragment {
     static final String TAG = "Android";
+
     FirebaseUser user;
     FirebaseFirestore db;
     DocumentReference userDoc;
+    FirebaseStorage storage;
+    StorageReference storageRef;
+    StorageReference picRef;
+
+    CircleImageView profilePicBtn;
     EditText usernameField;
     TextView emailField;
     EditText countryField;
@@ -37,15 +59,38 @@ public class ProfileFragment extends Fragment {
     Button logoutBtn;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         user = FirebaseAuth.getInstance().getCurrentUser();
         db = FirebaseFirestore.getInstance();
+        storage = FirebaseStorage.getInstance();
+        storageRef = storage.getReference();
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Set buttons
+        updateBtn = requireView().findViewById(R.id.updateBtn);
+        updateBtn.setOnClickListener(this::update);
+        logoutBtn = requireView().findViewById(R.id.logoutBtn);
+        logoutBtn.setOnClickListener(this::logout);
+        profilePicBtn = requireView().findViewById(R.id.profilePic);
+        profilePicBtn.setOnClickListener(this::changeProfilePic);
+
+        // Get user info and write to text fields
+        StorageReference picRef = storageRef.child("ProfilePics").child(user.getUid());
         userDoc = db.collection("users").document(user.getUid());
         userDoc.get().addOnCompleteListener(task -> {
             if (task.isSuccessful()) {
                 DocumentSnapshot document = task.getResult();
                 if (document.exists()) {
+
+                    // Update profile pic
+                    profilePicBtn = requireView().findViewById(R.id.profilePic);
+                    Glide.with(this).load(picRef).centerCrop().into(profilePicBtn);
 
                     // Update fields using firebase user doc
                     TextView userHeader = requireView().findViewById(R.id.username);
@@ -68,33 +113,36 @@ public class ProfileFragment extends Fragment {
                 Log.d(TAG, "get failed with ", task.getException());
             }
         });
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    public void onResume() {
+        super.onResume();
+        StorageReference picRef = storageRef.child("ProfilePics").child(user.getUid());
+        profilePicBtn = requireView().findViewById(R.id.profilePic);
+        Glide.with(this).load(picRef).centerCrop().into(profilePicBtn);
+    }
 
-        updateBtn = requireView().findViewById(R.id.updateBtn);
-        updateBtn.setOnClickListener(this::update);
-
-        logoutBtn = requireView().findViewById(R.id.logoutBtn);
-        logoutBtn.setOnClickListener(this::logout);
+    public void changeProfilePic(View view)
+    {
+        // Redirect to image upload activity
+        Intent intent = new Intent(getActivity(), ChangeProfilePic.class);
+        startActivity(intent);
     }
 
     public void update(View view) {
         String username = usernameField.getText().toString();
         String country = countryField.getText().toString();
 
-        // Update relevant fields in Firestore
+        // Update relevant fields in FirestoreDB
         db.collection("users").document(user.getUid()).update(
                 "username", username,
                 "country", country
         );
         Toast.makeText(getActivity(), "Information updated", Toast.LENGTH_SHORT).show();
-        getFragmentManager().beginTransaction().detach(this).attach(this).commit();
 
-        // Refreshing SearchActivity.java to show change to 'Passport Held'
+        // Refreshing and refocusing SearchActivity.java to show change to 'Passport Held'
         new Handler().post(new Runnable() {
             @Override
             public void run()
@@ -104,7 +152,6 @@ public class ProfileFragment extends Fragment {
                         | Intent.FLAG_ACTIVITY_NO_ANIMATION);
                 getActivity().overridePendingTransition(0, 0);
                 getActivity().finish();
-
                 getActivity().overridePendingTransition(0, 0);
                 startActivity(intent);
             }
