d7ce12ee8b428f4094fbcc48e977aadcc3915485
workunhard <workun.code@gmail.com>
2022-11-04 00:57:11 -0700

    Fixed profile pic image upload; updates Storage and profileFragment on ChangeProfile activity close.

--- app/src/main/java/com/example/planer/SearchActivity.java
+++ app/src/main/java/com/example/planer/SearchActivity.java
@@ -9,8 +9,6 @@ import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
