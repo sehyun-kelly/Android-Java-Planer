7621367efcdd21bb4dda2c7030d7f5e301d53f2d
Melanie Ta <nngoc2701@gmail.com>
2022-11-15 10:05:48 -0800

    Tidy up

--- app/src/main/java/com/example/planer/MainActivity.java
+++ app/src/main/java/com/example/planer/MainActivity.java
@@ -8,16 +8,8 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.planer.data.FirebaseDriver;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.FieldValue;
-import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
